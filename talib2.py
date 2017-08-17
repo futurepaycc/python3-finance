474f096dbb20706a4ed24b59ab0e6b4cfd491f8a
liunix <liyifan.com@gmail.com>
2017-08-17 16:17:50 +0800

    convert ipython log to taglib2.py

--- talib2.py
+++ talib2.py
@@ -1,15 +1,11 @@
-# IPython log file
-
-get_ipython().magic('logstart')
 import talib
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import tushare as ts
+
 df = ts.get_hist_data('sh',start='2017-01-01')
-df.columns
 df[df['volume'] == 0] 
-get_ipython().magic('pinfo talib.MACD')
 def myMACD(price, fastperiod=12, slowperiod=26, signalperiod=9):
     ewma12 = pd.ewma(price,span=fastperiod)
     ewma60 = pd.ewma(price,span=slowperiod)
@@ -17,24 +13,18 @@ def myMACD(price, fastperiod=12, slowperiod=26, signalperiod=9):
     dea = pd.ewma(dif,span=signalperiod)
     bar = (dif-dea) #有些地方的bar = (dif-dea)*2，但是talib中MACD的计算是bar = (dif-dea)*1
     return dif,dea,bar
+
 mydif,mydea,mybar = myMACD(df['close'].values, fastperiod=12, slowperiod=26, signalperiod=9)
 fig = plt.figure(figsize=[18,5])
-plt.plot(df.index,mydif,label='my dif')
-plt.plot(df.index,mydea,label='my dea')
-plt.plot(df.index,mybar,label='my bar')
-plt.legend(loc='best')
 plt.plot(mydif,label='my dif')
 plt.plot(mydea,label='my dea')
 plt.plot(mybar,label='my bar')
 plt.legend(loc='best')
-
 plt.show()
+
 df.index = pd.to_datetime(df.index) # 注意空虚数据转换，不然出错!
 plt.plot(df.index,mydif,label='my dif')
 plt.plot(df.index,mydea,label='my dea')
 plt.plot(df.index,mybar,label='my bar')
 plt.legend(loc='best')
-plt.show
 plt.show()
-get_ipython().magic('ls')
-get_ipython().magic('cat ipython_log.py')

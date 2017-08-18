34e37170b566763bcae32289db2d9e470197e2b5
liunix <liyifan.com@gmail.com>
2017-08-18 14:59:15 +0800

    tidy talib3.py

--- talib3.py
+++ talib3.py
@@ -13,8 +13,8 @@ dif,dea,bar = talib.MACD(df['close'].values, fastperiod=12, slowperiod=26, signa
 fig = plt.figure(figsize=[18,3])
 
 df.index = pd.to_datetime(df.index) # 注意空虚数据转换，不然出错!
-plt.plot(df.index,dif,label='my dif')
-plt.plot(df.index,dea,label='my dea')
-plt.plot(df.index,bar,label='my bar')
+plt.plot(df.index,dif,label='dif')
+plt.plot(df.index,dea,label='dea')
+plt.plot(df.index,bar,label='bar')
 plt.legend(loc='best')
 plt.show()

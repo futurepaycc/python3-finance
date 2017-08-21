d15e3f3b24f0c8ce0dae91a65d0fb4f26d8c1ec3
liunix <liyifan.com@gmail.com>
2017-08-21 15:47:03 +0800

    add comment to talib3.py

--- talib3.py
+++ talib3.py
@@ -10,14 +10,14 @@ df = pd.read_csv("sh.csv",index_col="date")
 df[df['volume'] == 0] 
 
 dif,dea,bar = talib.MACD(df['close'].values, fastperiod=12, slowperiod=26, signalperiod=9)
-fig = plt.figure(figsize=[18,3])
-
 df.index = pd.to_datetime(df.index) # 注意数据转换，不然出错!
-plt.plot(df.index,dif,label='dif')
-plt.plot(df.index,dea,label='dea')
-plt.plot(df.index,bar,label='bar')
-plt.legend(loc='best')
-plt.show()
+
+# fig = plt.figure(figsize=[18,3])
+# plt.plot(df.index,dif,label='dif')
+# plt.plot(df.index,dea,label='dea')
+# plt.plot(df.index,bar,label='bar')
+# plt.legend(loc='best')
+# plt.show()
 
 for x in range(len(df.index) - 1):
     deaItem = dea.tolist()[x]
